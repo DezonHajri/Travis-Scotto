5767075b7e44a2b94d3cbe75127f26e250e98932
Dezon Hajri <dezon.hajri@iismontalegenova.it>
2024-11-21 10:31:25 +0100

    FINALE

--- programma.js
+++ programma.js
@@ -12,6 +12,28 @@ for (const element of elementi) {
   elemento.style.left = 100 * (element.xpos - 1) + "px";
   elemento.style.top = 120 * (element.ypos - 1) + "px";
   elemento.style.backgroundColor = "#" + element["cpk-hex"];
+  elemento.style.boxSizing = "border-box"
+  elemento.style.borderWidth = "0.5px"
+
+  elemento.addEventListener("click", () => {
+      let immagineScheda = document.getElementById("immagine-scheda")
+      immagineScheda.src = element.image.url;
+      immagineScheda.style.borderstyle = "solid";
+
+      let titoloScheda = document.getElementById("titolo-scheda");
+      titoloScheda.innerText = element.name;
+
+      let descrizioneScheda = document.getElementById("descrizione-scheda")
+      descrizioneScheda.innerText = element.summary;
+
+      let categoriaScheda = document.getElementById("categoria-scheda")
+      categoriaScheda.innerText = "Category: " + element.category;
+
+      let densità = document.getElementById("densità")
+      densità.innerText = "density: " + element.density;
+
+  })
+
 
   let numeroAtomico = document.createElement("p");
   numeroAtomico.innerText = element.number;
@@ -19,8 +41,6 @@ for (const element of elementi) {
   numeroAtomico.style.textAlign = "left"
 
    
- 
-
 
   let simbolo = document.createElement("a");
   simbolo.innerText = element.symbol;
